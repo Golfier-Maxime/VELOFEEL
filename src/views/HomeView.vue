e8d694e3f8c7a25c77a7080ea2073e855fd01e6d
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-01 09:31:42 +0200

    Commit Init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <h1 class="text-white dark:text-black">COUCOu</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const withAnimations = require('animated-tailwindcss');
+
+module.exports = withAnimations({
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation:{
+      }
+    },
+    },
+  plugins: [],
+}
+);
