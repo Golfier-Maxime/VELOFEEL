30db90f0ad37a6133cc570327627a9f9c3a6951d
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-04 17:57:52 +0200

    page partenaires

--- src/views/partenaires.vue
+++ src/views/partenaires.vue
@@ -10,4 +10,22 @@
     <div class="flex justify-center mt-10">
         <img src="/images/Arrow_Down.svg" alt="">
     </div>
+    <div class="partenaires">
+        <img src="/images/logo_cannondale.png" alt="" class="w-[350px]">
+        <img src="/images/logo_Giant.png" alt="">
+        <img src="/images/logo_KTM.png" alt="">
+        <img src="/images/logo_Thule.png" alt="">
+        <img src="/images/logo_peugeot.png" alt="">
+        <img src="/images/logo_shimano.png" alt="">
+        <img src="/images/logo_pro.jpg" alt="">
+        <img src="/images/logo_lazer.png" alt="">
+        <img src="" alt="">
+        <img src="" alt="">
+        <p>dede</p>
+    </div>
 </template>
+<style scoped>
+.partenaires {
+    background: white;
+}
+</style>
