48335c0024be802033a3b5d46f6fd4e1912fc94f
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-05 15:59:06 +0200

    apropos fix

--- src/views/apropos.vue
+++ src/views/apropos.vue
@@ -1,10 +1,31 @@
 <script setup></script>
 
 <template>
-    <div class="">
-        <p
-            class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
-            TEST APROPOS</p>
+    <div class="lg:mx-20">
+        <div>
+            <h1
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                Vélofeel</h1>
+            <h2
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                Quelques informations</h2>
+        </div>
+        <div class="flex justify-center mt-10">
+            <img src="/images/Arrow_Down.svg" alt="">
+        </div>
+        <div class="flex gap-9">
+            <div class="text-Grey-Velofeel dark:text-Dark-Grey">
+                <h3 class="font-overpass font-bold text-[50px]">Vélofeel c'est quoi ?</h3>
+                <p class="font-OpenSans mr-28">Descrption : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus
+                    ultricies quam in magna
+                    congue vestibulum. Donec malesuada luctus dolor eu viverra. Nulla tincidunt facilisis sapien, non
+                    tristique mi volutpat quis. </p>
+            </div>
+            <div>
+                <img src="/images/logo_velofeel.png" alt="" class="w-[600px]">
+            </div>
+        </div>
     </div>
 </template>
 
