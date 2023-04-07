638e0a9923ec30868bd735d5f27e6915289a85c0
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-07 16:30:18 +0200

    architecture produit

--- src/views/produit.vue
+++ src/views/produit.vue
@@ -4,6 +4,7 @@
 
 <template>
     <div>
+        <!-- titre -->
         <div>
             <h1
                 class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
@@ -12,8 +13,16 @@
                 class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
                 Nos Produits</h2>
         </div>
+        <!-- fleche rouge qui montre en bas -->
         <div class="flex justify-center mt-10">
             <img src="/images/Arrow_Down.svg" alt="">
+        </div>
+        <!-- list produit -->
+        <div class="flex">
+            <!-- filtre -->
+            <div></div>
+            <!-- liste -->
+            <div></div>
         </div>
     </div>
 </template>
