0f306acb44d66c467517f3384a62bbc31f55d99a
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-08 10:04:50 +0200

    responsive mobile contact

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,13 +1,13 @@
 <script setup></script>
 <template>
-    <div class="lg:mx-20 text-Grey-Velofeel dark:text-Dark-Grey">
+    <div class="lg:mx-20 mx-4 text-Grey-Velofeel dark:text-Dark-Grey">
         <!-- titre -->
         <div>
             <h1
-                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-[50px] text-center font-overpass leading-tight">
                 Vélofeel</h1>
             <h2
-                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-4xl text-center font-overpass leading-tight">
                 Nous contactez</h2>
         </div>
         <!-- fleche rouge -->
@@ -15,37 +15,37 @@
             <img src="/images/Arrow_Down.svg" alt="">
         </div>
         <!-- sous titre -->
-        <div class="lg:mt-14">
-            <h3 class="text-Grey-Velofeel dark:text-Dark-Grey font-overpass font-extrabold lg:text-5xl">Nous contactez avec
+        <div class="lg:mt-14 mt-11">
+            <h3 class="text-Grey-Velofeel dark:text-Dark-Grey font-overpass font-extrabold lg:text-5xl text-2xl">Nous
+                contactez avec
                 nos
                 adresses
             </h3>
         </div>
         <!-- info -->
-        <div class="flex justify-evenly mt-11 font-OpenSans">
+        <div class="flex lg:flex-row flex-col justify-evenly mt-11 font-OpenSans">
             <!--Info Contact -->
             <div class="flex flex-col gap-3">
                 <div>
                     <p></p>
-                    <p>03 81 30 91 59</p>
+                    <p>Tel : 03 81 30 91 59</p>
                 </div>
                 <div>
                     <p></p>
                     <a href="mailto:contact.clients@velofeel.com">
-                        <p>contact.clients@velofeel.com</p>
+                        <p>Mail : contact.clients@velofeel.com</p>
                     </a>
                 </div>
                 <div>
                     <p></p>
-                    <p>Espace Lumière2, Boulevard Moïse Foglia 25400 Audincourt</p>
+                    <p>Lieu : Espace Lumière 2, Boulevard Moïse Foglia 25400 Audincourt</p>
                 </div>
             </div>
             <!-- Horraires -->
             <div>
-                <p class="font-bold text-[20px]">Horaires</p>
-                <div class="flex gap-4">
-
-                    <div>
+                <p class="font-bold text-[20px] mt-11 lg:mt-0">Horaires</p>
+                <div class="flex gap-4 ">
+                    <div class="mt-4 lg:mt-0">
                         <p>Lundi :</p>
                         <p>Mardi :</p>
                         <p>Mercredi :</p>
@@ -54,7 +54,7 @@
                         <p>Samedi :</p>
                         <p>Dimanche :</p>
                     </div>
-                    <div>
+                    <div class="mt-4 lg:mt-0">
                         <p>Fermé</p>
                         <p>09:30-12:00, 14:00-19:00</p>
                         <p>09:30-12:00, 14:00-19:00</p>
@@ -69,7 +69,10 @@
         </div>
         <!-- google maps -->
         <div>
-            <h4 class="mb-8 text-Grey-Velofeel dark:text-Dark-Grey font-overpass font-extrabold lg:text-5xl">Nous sommes ici
+            <h4
+                class="mb-8 mt-11 lg:mt-0 text-Grey-Velofeel dark:text-Dark-Grey font-overpass font-extrabold lg:text-5xl text-2xl">
+                Nous
+                sommes ici
                 !
             </h4>
             <img src="/images/googlemaps.jpg" alt="">
