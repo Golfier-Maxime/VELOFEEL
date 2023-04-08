e6f2cf5aca2a88a5933e04f41efdeeeb786d7434
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-08 09:52:38 +0200

    responsive mobile apropos

--- src/views/apropos.vue
+++ src/views/apropos.vue
@@ -1,14 +1,14 @@
 <script setup></script>
 
 <template>
-    <div class="lg:mx-20">
+    <div class="lg:mx-20 mx-4">
         <!-- titre -->
         <div>
             <h1
-                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-[50px] text-center font-overpass leading-tight">
                 Vélofeel</h1>
             <h2
-                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-4xl text-center font-overpass leading-tight">
                 Quelques informations</h2>
         </div>
         <!-- flehe rouge -->
@@ -16,10 +16,10 @@
             <img src="/images/Arrow_Down.svg" alt="">
         </div>
         <!-- présentation velofeel -->
-        <div class="flex gap-9">
+        <div class="mt-11 flex lg:flex-row flex-col-reverse gap-9">
             <div class="text-Grey-Velofeel dark:text-Dark-Grey">
-                <h3 class="font-overpass font-bold text-[50px]">Vélofeel c'est quoi ?</h3>
-                <p class="font-OpenSans mr-28">Descrption : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <h3 class="font-overpass font-bold lg:text-[50px] text-4xl mb-4 lg-mb-0">Vélofeel c'est quoi ?</h3>
+                <p class="font-OpenSans lg:mr-28">Descrption : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Phasellus
                     ultricies quam in magna
                     congue vestibulum. Donec malesuada luctus dolor eu viverra. Nulla tincidunt facilisis sapien, non
@@ -30,12 +30,12 @@
             </div>
         </div>
         <!-- barre de séparation -->
-        <div class="bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-16 lg:mx-20 w-[100hv]"></div>
+        <div class="mb-11 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-16 lg:mx-20 w-[100hv]"></div>
         <!-- autre -->
-        <div class="flex gap-9 mt-16">
+        <div class="flex lg:flex-row flex-col-reverse gap-9 lg:mt-11">
             <div class="text-Grey-Velofeel dark:text-Dark-Grey">
-                <h3 class="font-overpass font-bold text-[50px]">Vélofeel c'est quoi ?</h3>
-                <p class="font-OpenSans mr-28">Descrption : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                <h3 class="font-overpass font-bold lg:text-[50px] text-4xl mb-4 lg-mb-0">Vélofeel c'est quoi ?</h3>
+                <p class="font-OpenSans lg:mr-28">Descrption : Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     Phasellus
                     ultricies quam in magna
                     congue vestibulum. Donec malesuada luctus dolor eu viverra. Nulla tincidunt facilisis sapien, non
