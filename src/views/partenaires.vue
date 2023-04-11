f0d0401f44b8b103caa68a52e57c3c00f3a8992d
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-11 16:43:22 +0200

    list partenaires

--- src/views/partenaires.vue
+++ src/views/partenaires.vue
@@ -15,6 +15,7 @@
         <!-- présentation des partenaires -->
         <div class="partenaires rounded-lg shadow-md text-Dark-Grey font-OpenSans">
             <div class=" flex flex-col  gap-10 mx-4">
+                <!-- Partenaire -->
                 <div class="flex gap-10 justify-center">
                     <img src="/images/logo_cannondale.png" alt="" class="w-[300px]">
                     <div class="flex flex-col gap-2">
@@ -25,15 +26,91 @@
                 </div>
                 <!-- barre de séparation -->
                 <div class="mb-8 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-8 lg:mx-20 w-[100hv]"></div>
-                <img src="/images/logo_Giant.png" alt="" class="w-[300px]">
-                <img src="/images/logo_ktm2.jpg" alt="" class="w-[300px]">
-                <img src="/images/logo_Thule.png" alt="" class="w-[300px]">
-                <img src="/images/logo_peugeot.png" alt="" class="w-[300px]">
-                <img src="/images/logo_shimano.png" alt="" class="w-[300px]">
-                <img src="/images/logo_pro.jpg" alt="" class="w-[300px]">
-                <img src="/images/logo_lazer.png" alt="" class="w-[300px]">
-                <img src="" alt="">
-                <img src="" alt="">
+                <!--  -->
+                <!-- Partenaire -->
+                <div class="flex gap-10 justify-center">
+                    <img src="/images/logo_Giant.png" alt="" class="w-[300px]">
+                    <div class="flex flex-col gap-2">
+                        <a href="https://www.giant-bicycles.com/fr">Lien vers le site du partenaires en cliquant
+                            <span class="font-bold text-Red-Velofeel">ICI</span>.</a>
+                        <p>Giant est ... Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                </div>
+                <!-- barre de séparation -->
+                <div class="mb-8 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-8 lg:mx-20 w-[100hv]"></div>
+                <!--  -->
+                <!-- Partenaire -->
+                <div class="flex gap-10 justify-center">
+                    <img src="/images/logo_ktm2.jpg" alt="" class="w-[300px]">
+                    <div class="flex flex-col gap-2">
+                        <a href="https://www.ktm-bikes.at/">Lien vers le site du partenaires en cliquant
+                            <span class="font-bold text-Red-Velofeel">ICI</span>.</a>
+                        <p>KTM est ... Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                </div>
+                <!-- barre de séparation -->
+                <div class="mb-8 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-8 lg:mx-20 w-[100hv]"></div>
+                <!--  -->
+                <!-- Partenaire -->
+                <div class="flex gap-10 justify-center">
+                    <img src="/images/logo_Thule.png" alt="" class="w-[300px]">
+                    <div class="flex flex-col gap-2">
+                        <a href="https://www.thule.com/fr-fr/">Lien vers le site du partenaires en cliquant
+                            <span class="font-bold text-Red-Velofeel">ICI</span>.</a>
+                        <p>THULE est ... Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                </div>
+                <!-- barre de séparation -->
+                <div class="mb-8 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-8 lg:mx-20 w-[100hv]"></div>
+                <!--  -->
+                <!-- Partenaire -->
+                <div class="flex gap-10 justify-center">
+                    <img src="/images/logo_peugeot.png" alt="" class="w-[300px]">
+                    <div class="flex flex-col gap-2">
+                        <a href="https://cycles.peugeot.fr/">Lien vers le site du partenaires en cliquant
+                            <span class="font-bold text-Red-Velofeel">ICI</span>.</a>
+                        <p>Peugeot cycles est ... Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                </div>
+                <!-- barre de séparation -->
+                <div class="mb-8 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-8 lg:mx-20 w-[100hv]"></div>
+                <!--  -->
+                <!-- Partenaire -->
+                <div class="flex gap-10 justify-center">
+                    <img src="/images/logo_shimano.png" alt="" class="w-[300px]">
+                    <div class="flex flex-col gap-2">
+                        <a href="https://bike.shimano.com/fr-FR/home.html">Lien vers le site du partenaires en cliquant
+                            <span class="font-bold text-Red-Velofeel">ICI</span>.</a>
+                        <p>SHIMANO est ... Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                </div>
+                <!-- barre de séparation -->
+                <div class="mb-8 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-8 lg:mx-20 w-[100hv]"></div>
+                <!--  -->
+                <!-- Partenaire -->
+                <div class="flex gap-10 justify-center">
+                    <img src="/images/logo_pro.jpg" alt="" class="w-[300px]">
+                    <div class="flex flex-col gap-2">
+                        <a href="https://www.pro-bikegear.com/fr/">Lien vers le site du partenaires en cliquant
+                            <span class="font-bold text-Red-Velofeel">ICI</span>.</a>
+                        <p>SHIMANO est ... Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                </div>
+                <!-- barre de séparation -->
+                <div class="mb-8 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-8 lg:mx-20 w-[100hv]"></div>
+                <!--  -->
+                <!-- Partenaire -->
+                <div class="flex gap-10 justify-center">
+                    <img src="/images/logo_lazer.png" alt="" class="w-[300px]">
+                    <div class="flex flex-col gap-2">
+                        <a href="https://www.lazersport.com/fr/">Lien vers le site du partenaires en cliquant
+                            <span class="font-bold text-Red-Velofeel">ICI</span>.</a>
+                        <p>SHIMANO est ... Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    </div>
+                </div>
+                <!-- barre de séparation -->
+                <div class="mb-8 lg:mb-0 bg-Grey-Velofeel dark:bg-Dark-Grey rounded-sm h-1 mt-8 lg:mx-20 w-[100hv]"></div>
+                <!--  -->
             </div>
         </div>
     </div>
