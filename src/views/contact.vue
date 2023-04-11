a713faceb5a23ecab9cb773c6262910ca7ccf8e6
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-11 11:02:39 +0200

    contact static

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -68,43 +68,47 @@
 
         </div>
         <!-- google maps -->
-        <div>
-            <h4
-                class="mb-8 mt-11 lg:mt-0 text-Grey-Velofeel dark:text-Dark-Grey font-overpass font-extrabold lg:text-5xl text-2xl">
-                Nous
-                sommes ici
-                !
-            </h4>
-            <img src="/images/googlemaps.jpg" alt="">
-        </div>
-        <div>
-            <!-- CONTACT FORM HERE -->
-            <form action="https://api.web3forms.com/submit" method="POST" class="space-y-8">
-                <input type="hidden" name="access_key" value="6c199d9f-bd2b-40fb-b83f-538406f464a7">
-                <div>
-                    <label for="email"
-                        class="block mb-2 mt-4 lg:text-[24px] text-Grey-Velofeel dark:text-Dark-Grey duration-700">E-Mail</label>
-                    <input type="email" id="email"
-                        class="shadow-sm bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                        placeholder="Votre@adress.fr" required>
-                </div>
-                <div>
-                    <label for="subject" class="block mb-2 mt-4 lg:text-[24px]   dark:text-white duration-700">Sujet
-                        du mail</label>
-                    <input type="text" id="subject"
-                        class="block p-3 w-full mt-4 lg:text-[18px]  bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                        placeholder="Quelle est votre demande ?" required>
-                </div>
-                <div class="sm:col-span-2">
-                    <label for="message" class="block mb-2 mt-4 lg:text-[24px]   dark:text-white duration-700">Votre
-                        message</label>
-                    <textarea id="message" rows="6"
-                        class="block p-2.5 w-full mt-4 lg:text-[18px] bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="Message"></textarea>
-                </div>
-                <input type="hidden" name="redirect" value="https://web3forms.com/success">
-                <button type="submit" class="">Envoyer</button>
-            </form>
+        <div class="flex gap-4 flex-col lg:flex-row ">
+            <div>
+                <h4
+                    class="mb-8 mt-11 lg:mt-0 text-Grey-Velofeel dark:text-Dark-Grey font-overpass font-extrabold lg:text-5xl text-2xl">
+                    Nous
+                    sommes ici
+                    !
+                </h4>
+                <img src="/images/googlemaps.jpg" alt="">
+            </div>
+            <div class="lg:w-[50%]">
+                <!-- CONTACT FORM HERE -->
+                <form action="/" method="POST" class="">
+                    <input type="hidden" name="access_key" value="  ">
+                    <div>
+                        <label for="email"
+                            class="block mb-2 mt-4 text-Grey-Velofeel dark:text-Dark-Grey  duration-700">E-Mail</label>
+                        <input type="email" id="email"
+                            class="shadow-sm bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="Votre@adress.fr" required>
+                    </div>
+                    <div>
+                        <label for="subject"
+                            class="block mb-2 mt-4    text-Grey-Velofeel dark:text-Dark-Grey duration-700">Sujet
+                            du mail</label>
+                        <input type="text" id="subject"
+                            class="block p-3 w-full mt-4 text-sm  bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="Quelle est votre demande ?" required>
+                    </div>
+                    <div class="sm:col-span-2">
+                        <label for="message"
+                            class="block mb-2 mt-4 text-Grey-Velofeel dark:text-Dark-Grey   duration-700">Votre
+                            message</label>
+                        <textarea id="message" rows="6"
+                            class="block p-2.5 w-full mt-4  bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Message"></textarea>
+                    </div>
+                    <input type="hidden" name="redirect" value="https://web3forms.com/success">
+                    <button type="submit" class="">Envoyer</button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
