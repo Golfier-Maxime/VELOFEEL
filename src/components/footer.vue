4e1143e4b93f90200a712e98392d197b490d03d5
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-19 11:07:38 +0200

    fix

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -6,7 +6,7 @@ import LogoInsta from './logo/insta.vue'
 
 <template>
     <footer
-        class="text-Grey-Velofeel dark:text-Dark-Grey bg-Dark-Grey dark:bg-Grey-Velofeel duration-700 tracking-wide pt-12">
+        class="text-Grey-Velofeel dark:text-Dark-Grey bg-Dark-Grey dark:bg-Grey-Velofeel duration-700 tracking-wide pt-24">
         <div class="flex justify-center">
             <img src="/images/logo_velofeel.png" alt="Logo de vélofeel" class="">
         </div>
