765089811427d7daadeec89378f515a8a572112f
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-04-21 11:01:13 +0200

    fix

---
+++ src/views/CGU.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="lg:mx-20 mx-4">
+        <!-- titre -->
+        <div>
+            <h1
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                Vélofeel</h1>
+            <h2
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                CGU- Conditions générales d'utilisations</h2>
+        </div>
+        <div class=" flex justify-center mt-10">
+            <img src="/images/Arrow_Down.svg" alt="">
+        </div>
+    </div>
+</template>
