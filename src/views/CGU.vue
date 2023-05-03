e2f436ced5f6ebe0db6ce5dd9d97554b20754792
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-05-03 10:18:04 +0200

    fix + CGU

--- src/views/CGU.vue
+++ src/views/CGU.vue
@@ -3,14 +3,141 @@
         <!-- titre -->
         <div>
             <h1
-                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                class="text-Grey-Velofeel dark:text-Dark-Grey lg:text-[100px] text-[50px] font-extrabold  text-center font-overpass leading-tight">
                 Vélofeel</h1>
             <h2
-                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[80px] text-4xl text-center font-overpass leading-tight">
                 CGU- Conditions générales d'utilisations</h2>
         </div>
         <div class=" flex justify-center mt-10">
             <img src="/images/Arrow_Down.svg" alt="arrow down">
         </div>
+        <div class="mt-16 text-Grey-Velofeel dark:text-Dark-Grey font-OpenSans">
+            <p class="mt-2">Le présent document a pour objet de définir les modalités et conditions dans lesquelles d'une
+                part, VELOFEEL,
+                met à la disposition de ses utilisateurs le site, et les services disponibles
+                sur le site et d'autre part, la manière par laquelle l'utilisateur accède au site et utilise ses services.
+            </p>
+            <p class="mt-2">Toute connexion au site est subordonnée au respect des présentes conditions.</p>
+            <p class="mt-2">Pour l'utilisateur, le simple accès au site de Vélofeel à l'adresse URL suivante
+                https://velofeel.com/
+                implique l'acceptation
+                de l'ensemble des conditions décrites ci-après.</p>
+            <h2 class="mt-16">Propriété intellectuelle</h2>
+            <p class="mt-2">La structure générale du site https://velofeel.com/ , ainsi que les textes, graphiques, images,
+                sons et
+                vidéos la composant,
+                sont la propriété de l'éditeur ou de ses partenaires. Toute représentation et/ou reproduction et/ou
+                exploitation partielle ou totale des contenus et services proposés par le site https://velofeel.com/ , par
+                quelque procédé
+                que ce soit, sans l'autorisation préalable et par écrit de Vélofeel et/ou de ses partenaires est strictement
+                interdite et serait susceptible de constituer une contrefaçon au sens des articles L 335-2 et suivants du
+                Code de la propriété intellectuelle.
+
+                Les marques "Vélofeel ", "velofeel" sont des marques déposées par Vélofeel .Toute représentation et/ou
+                reproduction et/ou
+                exploitation partielle ou totale de ces marques, de quelque nature que ce soit, est totalement prohibée.</p>
+            <h2 class="mt-16">Liens hypertextes</h2>
+            <p class="mt-2">Le site https://velofeel.com/ peut contenir des liens hypertextes vers d'autres sites présents
+                sur le réseau
+                Internet.
+
+                Il est possible de créer un lien vers la page de présentation de ce site sans autorisation expresse de
+                Vélofeel. Aucune autorisation ou demande d'information préalable ne peut être exigée par Vélofeel à
+                l'égard d'un site qui souhaite établir un lien vers le site de vélofeel. Il convient toutefois d'afficher
+                ce site dans une nouvelle fenêtre du navigateur. Cependant, vélofeel se réserve le droit de demander la
+                suppression d'un lien qu'il estime non conforme à l'objet du site https://velofeel.com/</p>
+            <h2 class="mt-16">Responsabilité de l'éditeur</h2>
+            <p class="mt-2">Les informations et/ou documents figurant sur ce site et/ou accessibles par ce site proviennent
+                de sources
+                considérées comme étant fiables.</p>
+            <p class="mt-2">Toutefois, ces informations et/ou documents sont susceptibles de contenir des inexactitudes
+                techniques et des
+                erreurs typographiques.</p>
+            <p class="mt-2">Vélofeel se réserve le droit de les corriger, dès que ces erreurs sont portées à sa
+                connaissance.
+            </p>
+            <p class="mt-2">Il est fortement recommandé de vérifier l'exactitude et la pertinence des informations et/ou
+                documents mis à
+                disposition sur ce site.</p>
+            <p class="mt-2">Les informations et/ou documents disponibles sur ce site sont susceptibles d'être modifiés à
+                tout moment, et
+                peuvent avoir fait l'objet de mises à jour. En particulier, ils peuvent avoir fait l'objet d'une mise à jour
+                entre le moment de leur téléchargement et celui où l'utilisateur en prend connaissance.</p>
+            <p class="mt-2">L'utilisation des informations et/ou documents disponibles sur ce site se fait sous l'entière et
+                seule
+                responsabilité de l'utilisateur, qui assume la totalité des conséquences pouvant en découler, sans que
+                Vélofeel puisse être recherché à ce titre, et sans recours contre ce dernier.</p>
+            <p class="mt-2">Vélofeel ne pourra en aucun cas être tenu responsable de tout dommage de quelque nature qu'il
+                soit résultant
+                de l'interprétation ou de l'utilisation des informations et/ou documents disponibles sur ce site.</p>
+            <h2 class="mt-16">Accès au site</h2>
+            <p class="mt-2">Vélofeel s'efforce de permettre l'accès au site 24 heures sur 24, 7 jours sur 7, sauf en cas de
+                force
+                majeure ou d'un événement hors du contrôle de Vélofeel, et sous réserve des éventuelles pannes et
+                interventions de maintenance nécessaires au bon fonctionnement du site et des services.</p>
+            <p class="mt-2">Par conséquent, Vélofeel ne peut garantir une disponibilité du site et/ou des services, une
+                fiabilité des
+                transmissions et des performances en terme de temps de réponse ou de qualité. Il n'est prévu aucune
+                assistance
+                technique vis à vis de l'utilisateur que ce soit par des moyens électronique ou téléphonique.</p>
+            <p class="mt-2">La responsabilité de Vélofeel ne saurait être engagée en cas d'impossibilité d'accès à ce site
+                et/ou
+                d'utilisation des services.</p>
+            <p class="mt-2">Par ailleurs, Vélofeel peut être amené à interrompre le site ou une partie des services, à tout
+                moment sans
+                préavis, le tout sans droit à indemnités. L'utilisateur reconnaît et accepte que Vélofeel ne soit pas
+                responsable des interruptions, et des conséquences qui peuvent en découler pour l'utilisateur ou tout tiers.
+            </p>
+            <h2 class="mt-16">Modification des conditions d'utilisation</h2>
+            <p class="mt-2">Vélofeel se réserve la possibilité de modifier, à tout moment et sans préavis, les présentes
+                conditions
+                d'utilisation afin de les adapter aux évolutions du site et/ou de son exploitation.</p>
+            <h2 class="mt-16">Règles d'usage d'Internet</h2>
+            <p class="mt-2">L'utilisateur déclare accepter les caractéristiques et les limites d'Internet, et notamment
+                reconnaît que :
+            </p>
+            <p class="mt-2">Vélofeel n'assume aucune responsabilité sur les services accessibles par Internet et n'exerce
+                aucun contrôle
+                de quelque forme que ce soit sur la nature et les caractéristiques des données qui pourraient transiter par
+                l'intermédiaire de son centre serveur.</p>
+            <p class="mt-2">L'utilisateur reconnaît que les données circulant sur Internet ne sont pas protégées notamment
+                contre les
+                détournements éventuels. La présence du logo Vélofeel institue une présomption simple de validité. La
+                communication de toute information jugée par l'utilisateur de nature sensible ou confidentielle se fait à
+                ses risques et périls.</p>
+            <p class="mt-2">L'utilisateur reconnaît que les données circulant sur Internet peuvent être réglementées en
+                termes d'usage ou
+                être protégées par un droit de propriété.</p>
+            <p class="mt-2">L'utilisateur est seul responsable de l'usage des données qu'il consulte, interroge et transfère
+                sur
+                Internet.</p>
+            <p class="mt-2">L'utilisateur reconnaît que Vélofeel ne dispose d'aucun moyen de contrôle sur le contenu des
+                services
+                accessibles sur Internet</p>
+            <h2 class="mt-16">Droit applicable</h2>
+            <p class="mt-2">Tant le présent site que les modalités et conditions de son utilisation sont régis par le droit
+                français,
+                quel que soit le lieu d'utilisation. En cas de contestation éventuelle, et après l'échec de toute tentative
+                de recherche d'une solution amiable, les tribunaux français seront seuls compétents pour connaître de ce
+                litige.</p>
+            <p class="mt-2">Pour toute question relative aux présentes conditions d'utilisation du site, vous pouvez nous
+                écrire à
+                l'adresse suivante : 2 Boulevard Moïse Foglia 25400 Audincourt</p>
+            <p class="mt-2">" Les présentes conditions générales (CGU) sont une reproduction du Modèle de CGU (conditions
+                générales
+                d'utilisation) d'un site Internet, dont le titulaire des droits d'auteur est DROITISSIMO.COM, site internet
+                d'information juridique grand public. "</p>
+        </div>
     </div>
 </template>
+
+<style scoped>
+h2 {
+    font-weight: bold;
+}
+
+h3 {
+    font-weight: bold;
+}
+</style>
--- src/views/legal.vue
+++ src/views/legal.vue
@@ -6,10 +6,10 @@
         <!-- titre -->
         <div>
             <h1
-                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                class="text-Grey-Velofeel dark:text-Dark-Grey lg:text-[100px] text-[50px] font-extrabold  text-center font-overpass leading-tight">
                 Vélofeel</h1>
             <h2
-                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[100px] text-center font-overpass leading-tight">
+                class="text-Grey-Velofeel dark:text-Dark-Grey font-extrabold lg:text-[80px] text-4xl text-center font-overpass leading-tight">
                 Mentions Légales</h2>
         </div>
         <div class=" flex justify-center mt-10">
