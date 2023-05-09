463daac846b4605ed0b6a7bd50c01f2c3b33ef8b
Maxime Golfier <maxime.golfier@edu.univ-fcomte.fr>
2023-05-09 10:18:39 +0200

    fix api facebook

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -567,8 +567,8 @@ export default {
                 Nous retrouver sur nos réseaux</h4>
             <div class="lg:mt-16 mt-4 flex flex-col lg:flex-row justify-center lg:gap-16 gap-4">
                 <div class="flex justify-center">
-                    <iframe class="lg:w-[500px] lg:h-[500px] h-[300px] w-[320px]" title="Facebook"
-                        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FVELOFEEL&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"></iframe>
+                    <iframe class="lg:w-[500px] lg:h-[500px] h-[300px] w-[350px]" title="Facebook"
+                        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FVELOFEEL&tabs=timeline&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"></iframe>
                 </div>
                 <div>
                     <div class="">
